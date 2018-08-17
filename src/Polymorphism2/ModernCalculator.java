bb2168b771f1912683a4127ee5eef0c02ba0fac1
RoySubhra1988 <roysubhra1988@gmail.com>
2018-08-17 17:09:46 -0400

    commit 2

--- src/Polymorphism2/ModernCalculator.java
+++ src/Polymorphism2/ModernCalculator.java
@@ -1,12 +1,24 @@
 package Polymorphism2;
 
 public class ModernCalculator extends Calculator {
-	
-@Override
-public int CalcArea(int a, int b, int c, int d) {
-		
-		int total = a+b+c+d-5;
+	@Override
+	public int CalcArea(int a, int b, int c, int d) {
+
+		int total = (a + b + c + d)- 5;
 		return total;
-		
+
+	}
+	@Override
+	public double CalcArea(double a, int b, int c, int d) {
+
+		double total = (a + b + c + d) - .2;
+		return total;
 }
+	@Override
+	public int CalcArea(int a, int b) {
+		int total = a+b +5;
+		return total;
+	}
+
+		
 }
--- src/Polymorphism2/TestCalculator.java
+++ src/Polymorphism2/TestCalculator.java
@@ -7,11 +7,13 @@ public class TestCalculator {
 		System.out.println(calArea.CalcArea(3, 9));
 		System.out.println(calArea.CalcArea(8, 6, 5));
 		System.out.println(calArea.CalcArea(5,6, 6, 8));
+		System.out.println(calArea.CalcArea(5.2, 3, 4, 5));
 		
 		
 		ModernCalculator mc = new ModernCalculator();
-	System.out.println(mc.CalcArea(5, 6, 6, 8));
-		
+	System.out.println(mc.CalcArea(5.2, 3, 4, 5));
+	System.out.println(mc.CalcArea(10, 32, 7, 8));
+	System.out.println(mc.CalcArea(10, 32));	
 
 	}
 
